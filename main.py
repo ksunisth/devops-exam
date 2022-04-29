c3745d8361c7898229ba00ba322c9ba0a530e037
Sunisth Kumar <58183198+ksunisth@users.noreply.github.com>
2022-04-29 09:37:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+course_1 = "Marketing Strategies And Planning  (CSET310)"
+credits_1 = 3
+print("Course: ", course_1)
+print("Credit: ", credits_1)
+print("")
+course_2 = "Career Skills For IT Companies  (CSET483)"
+credits_2 = 3
+print("Course: ", course_2)
+print("Credit: ", credits_2)
+print("")
+course_3 = "DEEP LEARNING  (ECSE440L)"
+credits_3 = 3
+print("Course: ", course_3)
+print("Credit: ", credits_3)
+print("")
+course_4 = "DevOps Engineering Practice  (ECSE460L)"
+credits_4 = 3
+print("Course: ", course_4)
+print("Credit: ", credits_4)
+print("")
+course_5 = "ENVIRONMENT AND SUSTAINABILITY  (ESEA301L)"
+credits_5 = 3
+print("Course: ", course_5)
+print("Credit: ", credits_5)
+print("")
+
